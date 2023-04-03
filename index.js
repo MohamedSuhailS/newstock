33aa670b4e0dd6403f93a3997a5a6dd372090c93
MohamedSuhailS <129146372+MohamedSuhailS@users.noreply.github.com>
2023-04-03 14:07:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -120,32 +120,33 @@ const storage = multer.diskStorage({
 })
 
 app.post('/imageupload', async (req, res) => {	
-  try {
-      // 'avatar' is the name of our file input field in the HTML form
+   console.log(classifiedsadd)
+//   try {
+//       // 'avatar' is the name of our file input field in the HTML form
 
-      let upload = multer({ storage: storage}).single('avatar');
+//       let upload = multer({ storage: storage}).single('avatar');
 
-      upload(req, res, function(err) {
-          // req.file contains information of uploaded file
-          // req.body contains information of text fields
+//       upload(req, res, function(err) {
+//           // req.file contains information of uploaded file
+//           // req.body contains information of text fields
 
-          if (!req.file) {
-              return res.send('Please select an image to upload');
-          }
-          else if (err instanceof multer.MulterError) {
-              return res.send(err);
-          }
-          else if (err) {
-              return res.send(err);
-          }
+//           if (!req.file) {
+//               return res.send('Please select an image to upload');
+//           }
+//           else if (err instanceof multer.MulterError) {
+//               return res.send(err);
+//           }
+//           else if (err) {
+//               return res.send(err);
+//           }
 
-          const classifiedsadd = {
-      image: req.file.filename
-    };
-    console.log(classifiedsadd)
-      res.json({ success: 1 })       
-      }); 
-  }catch (err) {console.log(err)}
+//           const classifiedsadd = {
+//       image: req.file.filename
+//     };
+//     console.log(classifiedsadd)
+//       res.json({ success: 1 })       
+//       }); 
+//   }catch (err) {console.log(err)}
 })
 
 
